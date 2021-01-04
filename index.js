e4c5200ab68cfbf75b4ad202ab2fb9fab5666a23
Garrett Kuhlmann <garrett.kuhlmann@gmail.com>
2021-01-04 16:48:13 -0500

    server going with cleanup

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 var cors = require("cors");
 
-const app = require("express")(),
-  session = require("express-session");
+const app = require("express")();
 
 app.use(cors());
 http = require("http").createServer(app);
-
-// io = require('socket.io')(http);
-//   MongoStore = require('connect-mongo')(session);
-
-// app.use(
-//   session({
-//     store: new MongoStore({
-//       url: process.env.DATABASE_URL,
-//     }),
-//     secret: process.env.COOKIE_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24 * 7 * 1, // 1 week
-//     },
-//   }),
-// );
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
-// });
-
-// io.on('connection', (socket) => {
-//   socket.on('chat message', (msg) => {
-//     console.log('message: ' + msg);
-//     io.emit('chat message', msg);
-//   });
-// });
 
 // An api endpoint that returns a list of available tables
 app.get("/tables", (req, res) => {
