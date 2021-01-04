284b63d6f7262a9b4e2c0c9ffe70ac8499039186
Garrett Kuhlmann <garrett.kuhlmann@gmail.com>
2021-01-03 22:42:43 -0500

    blah

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const app = require('express')(),
-  session = require('express-session'),
-  http = require('http').createServer(app),
-  io = require('socket.io')(http);
+const app = require("express")(),
+  session = require("express-session"),
+  http = require("http").createServer(app);
+// io = require('socket.io')(http);
 //   MongoStore = require('connect-mongo')(session);
 
 // app.use(
@@ -20,27 +20,27 @@ const app = require('express')(),
 //   }),
 // );
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
 
 // An api endpoint that returns a list of available tables
-app.get('/api/getTables', (req, res) => {
-  const list = ['Garretts Room', 'High Rollers', 'Test Table', 'Mikis Table'];
+app.get("/api/getTables", (req, res) => {
+  const list = ["Garretts Room", "High Rollers", "Test Table", "Mikis Table"];
   res.json(list);
-  console.log('Sent list of tables');
+  console.log("Sent list of tables");
 });
 
 http.listen(5000, () => {
-  console.log('listening on *:5000');
+  console.log("listening on *:5000");
 });
