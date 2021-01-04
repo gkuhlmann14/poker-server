e97b88fc60b77d19319a8d2338a4f252ddc86792
Garrett Kuhlmann <garrett.kuhlmann@gmail.com>
2021-01-03 23:10:48 -0500

    move to index.js

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-/* eslint-disable no-global-assign */
+require("dotenv").config();
 
-require = require("esm")(module);
-module.exports = require("./main.js");
+const app = require("express")();
+// import session from "express-session";
+const http = require("http").createServer(app);
+// io = require('socket.io')(http);
+//   MongoStore = require('connect-mongo')(session);
+
+// app.use(
+//   session({
+//     store: new MongoStore({
+//       url: process.env.DATABASE_URL,
+//     }),
+//     secret: process.env.COOKIE_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24 * 7 * 1, // 1 week
+//     },
+//   }),
+// );
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
+
+// An api endpoint that returns a list of available tables
+app.get("/getTables", (req, res) => {
+  const list = ["Garretts Room", "High Rollers", "Test Table", "Mikis Table"];
+  res.json(list);
+  console.log("Sent list of tables");
+});
+
+http.listen();
